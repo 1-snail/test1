59a99112362f23b9b5c95fe43317a221150d47d0
one-snail <daijl9@163.com>
2020-08-27 10:35:17 +0800

    添加了忽略

---
+++ class/s1/ConsoleApplication1/MyLib/MyNameSpace2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib.MyNamSpace;
+
+namespace MyLib.MyNameSpace2
+{
+    class Class1
+    {
+        public Calculator calculator { get; set; }
+    }
+}
---
+++ class/s12/HelloOOP/HelloOOP/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOOP
+{
+     class Program
+    {
+        static void Main(string[] Args)
+        {
+            Type t = typeof(Car);
+            Type tb = t.BaseType;
+            Console.WriteLine(tb.FullName );
+   
+        }
+    }
+    class Vehicle
+    {
+        
+    }
+    class Car :Vehicle
+    {
+
+    }
+
+}
